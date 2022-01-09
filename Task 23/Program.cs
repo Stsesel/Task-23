d173cf46cc63a504ef40a8ad9557f61d88d804d7
asvartgroupspb@gmail.com <asvartgroupspb@gmail.com>
2022-01-09 20:51:39 +0300

    Добавьте файлы проекта.

---
+++ Task 23/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число, для которого необходимо вычислить факториал");
+            int n = Convert.ToInt32(Console.ReadLine());
+            FactAsync(n);
+            Console.ReadKey();
+        }
+        static void Fact(int n)
+        {
+            int factorial = 1;  
+            for (int i = 2; i <= n; i++)
+            {
+                factorial = factorial * i;
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Факториал числа числа " + n + " равен " + factorial);
+        }
+
+        static async void FactAsync(int n)
+        {
+            await Task.Run(()=>Fact(n));
+        }
+    }
+}
